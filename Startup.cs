7109e94c2cc05727fa1aa672ae8cbcd03c6de68a
krysmathis <krysmathis@gmail.com>
2018-01-24 12:02:20 -0600

    initial db migration

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using BangazonAPI.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace BangazonAPI
 {
@@ -24,6 +26,10 @@ namespace BangazonAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            string path = System.Environment.GetEnvironmentVariable("BANGAZON_API_DB");
+            var connection = $"Filename={path}";
+            Console.WriteLine($"connection = {connection}");
+            services.AddDbContext<BangazonAPIContext>(options => options.UseSqlite(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
