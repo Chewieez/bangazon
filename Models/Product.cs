d6608a4b3f4dead6803d78138c2d65f4dea2a121
krysmathis <krysmathis@gmail.com>
2018-01-25 09:33:39 -0600

    product and productcategory models

---
+++ Models/Product.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BangazonAPI.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        
+        [Required]
+        public int ProductCategoryId {get; set;}
+        public ProductCategory ProductCategory {get; set;}
+
+        public int CustomerId {get; set;}
+
+
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+        
+        [Required]
+        public string Description { get; set; }
+        
+        [Required]
+        public int Quantity { get; set; }
+
+
+    }
+}
