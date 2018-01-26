55b2f134c765d7ae5d104200d46f8a4d8a30696b
Dre mf Randaci <drerandaci@gmail.com>
2018-01-26 13:29:00 -0600

    boilerplate code for returning products array with orders

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -2,43 +2,58 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BangazonAPI.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BangazonAPI.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
-        // GET api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+        private readonly BangazonAPIContext _context;
 
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
+        // GET api/Orders
+        public IEnumerable<OrdersDto> GetOrdersAndProducts()
         {
-        }
+            var orders = _context
+                .Order
+                .Include("Product")
+                .Select(o => new OrdersDto
+                {
+                    OrderId = o.OrderId,
+                    CustomerId = o.CustomerId,
+                    PaymentTypeId = o.PaymentTypeId,
+                    OrderProducts = _context.Product.Select(p => new ProductDto
+                    {
+                        ProductId = p.ProductId,
+                        Price = p.Price,
+                        Quantity = p.Quantity,
+                        Name = p.Name
+                    })
+                }).ToList();
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
+            return orders;
         }
+    }
+    public class OrdersDto
+    {
+        public int OrderId { get; set; }
+        public int CustomerId { get; set; }
+        public int? PaymentTypeId { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<ProductDto> OrderProducts { get; set; }
+    }
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+    public class ProductDto
+    {
+        public int ProductId { get; set; }
+
+        public double Price { get; set; }
+                    
+        public int Quantity { get; set; }
+
+        public string Name { get; set; }
     }
 }
