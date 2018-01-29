357c6604170e915f130d07126f9e67220970b0c3
Jason Figueroa <jason.figueroa2@gmail.com>
2018-01-29 13:27:46 -0600

    working on commenting OrderController.cs

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -20,25 +20,66 @@ namespace BangazonAPI.Controllers
             _context = ctx;
         }
 
-        // GET api/values
+        /*
+            Author: Jason Figueroa
+            URL: GET api/order
+            Description:
+            Returns the order values from the database
+            Example GET response:
+            [
+                {
+                    "orderId": 1,
+                    "paymentTypeId": 1,
+                    "customerId": 1,
+                    "customer": null,
+                    "completedDate": "2017-07-17T00:00:00"
+                },
+                {
+                    "orderId": 2,
+                    "paymentTypeId": 16,
+                    "customerId": 16,
+                    "customer": null,
+                    "completedDate": "2017-12-09T00:00:00"
+                }
+            ]
+        */
         [HttpGet]
         public IActionResult Get()
         {
-            
+            // from the BangazonAPIContext object, retrieve the Order table
             var order = _context.Order.ToList();
 
             if (order == null)
             {
                 return NotFound();
             }
+
+            // values will be in JSON format
             return Ok(order);
 
         }
 
-        // GET api/values/5
+        /*
+            Author: Jason Figueroa
+            URL: GET api/order/{id}
+            Description:
+            Returns a specific order based on OrderId
+            Example GET response for "/api/order/1":
+            {
+                "orderId": 1,
+                "paymentTypeId": 1,
+                "customerId": 1,
+                "customer": null,
+                "completedDate": "2017-07-17T00:00:00"
+            }
+        */
         [HttpGet("{id}", Name="GetSingleOrder")]
         public IActionResult Get(int id)
         {
+            /*
+                This condition validates the values in model binding.
+                In this case, it validates that the id value is an integer.
+             */
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
@@ -46,6 +87,7 @@ namespace BangazonAPI.Controllers
 
             try
             {
+                // from the BangazonAPIContext object, retrieve the a single order record
                 Order order = _context.Order.Single(t => t.OrderId == id);
 
                 if (order == null)
@@ -53,6 +95,7 @@ namespace BangazonAPI.Controllers
                     return NotFound();
                 }
 
+                // values will be in JSON format
                 return Ok(order);
             }
             catch (System.InvalidOperationException ex)
@@ -61,7 +104,31 @@ namespace BangazonAPI.Controllers
             }
         }
 
-        // POST api/values
+        /*
+            Author: Jason Figueroa
+            URL: POST api/order/
+            Description:
+            This method handles post requests, which adds a
+            record to the database. When executing the POST request, do not
+            include the orderId in the body of the request. The database will
+            assign a uniqure OrderId.
+            Example POST body:
+            {
+                "name": "MacBook Air",
+                "serialNumber": "10688",
+                "purchaseDate": "2017-01-29T00:00:00",
+                "decommissionDate": null
+            }
+            
+            Assuming the newly created id is 24, the return value is:
+            {
+                "orderId": 24,
+                "name": "MacBook Air",
+                "serialNumber": "10688",
+                "purchaseDate": "2017-01-29T00:00:00",
+                "decommissionDate": null
+            }
+         */
         [HttpPost]
         public IActionResult Post([FromBody]Order order)
         {
